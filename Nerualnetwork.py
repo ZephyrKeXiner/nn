aa5208730573ffac3550e0fa50899418dad4bbfe
hanab1saku <aruiskyhappy2@outlook.com>
2023-04-02 17:22:02 +0800

    finish network

--- Nerualnetwork.py
+++ Nerualnetwork.py
@@ -3,22 +3,46 @@ import scipy.special
 
 
 class NerualNetwork():
-    def __init__(self,input_nodes,output_nodes,hidden_nodes,learning_rate):
-        self.input_nodes = input_nodes
-        self.output_nodes = output_nodes
-        self.hidden_nodes = hidden_nodes
+    def __init__(self,inlayer_nodes, outlayer_nodes, hiddenlayer_nodes, learning_rate):
+        self.inlayer_nodes = inlayer_nodes
+        self.outlayer_nodes = outlayer_nodes
+        self.hiddenlayer_nodes = hiddenlayer_nodes
         self.lrt = learning_rate
 
-        self.inputweight = np.random.normal(0.0,5.0,(self.hidden_nodes,self.input_nodes))
-        self.hiddenweight = np.random.normal(0.0,5.0,(self.output_nodes,self.hidden_nodes))
+        self.inputweight = np.random.normal(0.0,5.0,(self.hiddenlayer_nodes, self.inlayer_nodes))
+        self.hiddenweight = np.random.normal(0.0,5.0,(self.outlayer_nodes, self.hiddenlayer_nodes))
 
         self.activity_func = lambda x:scipy.special.expit(x)
     
     def forward_spread(self,inputs):
         inputs = np.array(inputs,ndmin=2).T
-        hidden_inputs = np.dot(self.inputweight,inputs)
-        hidden_outputs = self.activity_func(hidden_inputs)
-        output_inputs = np.dot(self.hiddenweight,hidden_outputs)
-        output_outputs = self.activity_func(output_inputs)
+        hiddenlayer_inputs = np.dot(self.inputweight, inputs)
+        hiddenlayer_outputs = self.activity_func(hiddenlayer_inputs)
+        outlayer_inputs = np.dot(self.hiddenweight, hiddenlayer_outputs)
+        outlayer_outputs = self.activity_func(outlayer_inputs)
 
-        print(output_outputs)
+        return inputs,hiddenlayer_outputs,outlayer_outputs
+
+    def error_comp(self,outlayer_outputs,correct):
+        correct = np.array(correct,ndmin=2).T
+        outlayer_error = correct - outlayer_outputs
+        hiddenlayer_error = np.dot(self.hiddenweight.T, outlayer_error)
+        return hiddenlayer_error, outlayer_error
+    
+    def gradintdecrease(self, inlayer, hiddenlayer_errors, output_errors, hiddenlayer_outputs, outlayer_outputs):
+
+        self.hiddenweight += self.lrt * np.dot((output_errors * outlayer_outputs * (1.0 - outlayer_outputs)),
+                                                np.transpose(hiddenlayer_outputs))
+        self.inputweight += self.lrt * np.dot((hiddenlayer_errors * hiddenlayer_outputs * (1.0 - hiddenlayer_outputs)),
+                                               np.transpose(inlayer))
+
+    def show(self,inputs):
+        inputs = np.array(inputs,ndmin=2).T
+
+        hidden_inputs = np.dot(self.inputweight, inputs)
+        hidden_outputs = self.activity_func(hidden_inputs)
+
+        final_inputs = np.dot(self.hiddenweight, hidden_outputs)
+        final_outputs = self.activity_func(final_inputs)
+
+        return final_outputs
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import Nerualnetwork
+import numpy as np
+from PIL import Image
 
-input_nodes = 5
-hidden_nodes =10
-out_puts = 3
-input_list =[4,2,5,2,4]
+input_nodes = 784
+hidden_nodes =1000
+out_puts = 10
+learning_rate = 0.3
+correct = []
+epoches = 12
 
-nn = Nerualnetwork.NerualNetwork(input_nodes,hidden_nodes,out_puts,0.3)
 
-nn.forward_spread(input_list)
+nn = Nerualnetwork.NerualNetwork(input_nodes,hidden_nodes,out_puts,learning_rate)
+for i in (epoches):
+    inputs, hiddenlayer_outputs, outlayer_outputs = nn.forward_spread(input_list)
+    hiddenlayer_error, outlayer_error = nn.error_comp(outlayer_outputs,correct)
+    nn.gradintdecrease(inputs, 
+                       hiddenlayer_error,
+                       outlayer_error,
+                       hiddenlayer_outputs,
+                       outlayer_outputs)
+
+final = nn.show()
